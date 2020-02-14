3f8c35fa797ee12a7499a0efbdaccd0c92e9662e
cwalbanese <631gti@gmail.com>
2020-02-14 14:23:39 -0500

    Readying for deployment
    Performed testing and fixed code for Heroku deployment

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
-const portfoliosController = require('../controllers/portfolios');
-const usersController = require('../controllers/users');
+const portfoliosController = require('./controllers/portfolios');
+const usersController = require('./controllers/users');
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/api/portfolios/', portfoliosController);
-app.use('/api/users/', usersController);
+app.use('/api/portfolios', portfoliosController);
+app.use('/api/users', usersController);
 
 app.set('port', process.env.PORT || 8080);
 
